d425d5a43f49f9130d5359e6207e5af157bd93bf
Eduard Keilholz <eduard.keilholz@loddersoftware.com>
2019-03-29 12:14:43 +0100

    Final states step 2

--- Frontend/src/app/pages/home/homepage/homepage.component.ts
+++ Frontend/src/app/pages/home/homepage/homepage.component.ts
@@ -8,36 +8,7 @@ import { ContactDto } from 'src/app/models/contact.models';
   styleUrls: ['./homepage.component.scss']
 })
 export class HomepageComponent implements OnInit {
-  public contactData: Array<ContactDto> = [
-    new ContactDto({
-      businessEntityID: 293,
-      firstName: 'Catherine',
-      lastName: 'Abel',
-      middleName: 'R.',
-      telephoneNumber: '206-555-2222',
-      street: 'P.O Box 5',
-      city: 'Edmonds',
-      stateProvince: 'WA',
-      postalCode: '98431',
-      countryRegion: 'USA',
-      eMailAddress: 'Joe@sample.com',
-      modifiedDate: new Date('2015-04-15T16:33:33.077')
-    }),
-    new ContactDto({
-      businessEntityID: 295,
-      firstName: 'Kim',
-      lastName: 'Abercrombie',
-      middleName: null,
-      telephoneNumber: '605-555-9877',
-      street: '990 5th Avenue',
-      city: 'Redmond',
-      stateProvince: 'WA',
-      postalCode: '98052',
-      countryRegion: 'USA',
-      eMailAddress: 'Customer3@sample.com',
-      modifiedDate: new Date('2015-04-15T16:33:33.077')
-    })
-  ];
+  public contactData: Array<ContactDto>;
   displayedColumns: string[] = [
     'businessEntityID',
     'firstName',
