eff29276cc9f44beb856ed5e638e7036cf6e853c
Eduard Keilholz <eduard.keilholz@loddersoftware.com>
2019-03-29 11:01:22 +0100

    Step 01 - Setting up the project

---
+++ Backend/FilterDemoApp/Data/AdvDataContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FilterDemoApp.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace FilterDemoApp.Data
+{
+    public class AdvDataContext : DbContext
+    {
+
+        public DbSet<ContactInfoEntity> Contacts { get; set; }
+
+        public AdvDataContext(DbContextOptions<AdvDataContext> options)
+            : base(options)
+        {
+        }
+
+    }
+}
---
+++ Frontend/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeModule } from './pages/home/home.module';
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    HomeModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
